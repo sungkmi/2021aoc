b35a4f5f16a81cdf0c1af82dbf76669fcf9e4809
sungkmi <sungkmi@gmail.com>
2022-01-28 20:02:11 +0900

    Day 8

---
+++ src/test/scala/lascala/aoc2021/day1_10/day8/Day8Test.scala
@@ -0,0 +1,33 @@
+package lascala.aoc2021.day1_10.day8
+
+import minitest.SimpleTestSuite
+import hedgehog.minitest.HedgehogSupport
+import hedgehog.*
+
+object Day8Test extends SimpleTestSuite with HedgehogSupport:
+
+  val testInput = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
+
+  example("day8 - solve1") {
+    solve1(testInput) ==== 26
+  }
+
+  example("day8 - decode") {
+    val line = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+    decode(line) ==== 5353
+  }
+
+  example("day8 - solve2") {
+    solve2(testInput) ==== 61229
+  }
+
+end Day8Test
